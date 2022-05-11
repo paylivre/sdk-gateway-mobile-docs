b1d7be06fdd745865b82b3c461e923023f4f31f9
MFAP-1 <manoelpereiraeng@gmail.com>
2022-05-11 10:49:01 -0300

    initial configs and layout

---
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    primary: "#215aff",
+    background: "#FFFFFF",
+    shape: `#F2F2FA`,
+    title: `#3D3D4D`,
+    text: `#6C6C80`,
+    components: {
+      blockquote: {
+        background: `#332616`,
+        text: `#E1E1E6`,
+      },
+    },
+  },
+  fonts: {
+    fontFace: {
+      family: "Open Sans",
+      //   file: "/fonts/openSans-regular-webfont",
+    },
+    body: "Open Sans",
+  },
+};
